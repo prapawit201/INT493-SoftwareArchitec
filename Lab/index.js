15c084bf43a04288fe534bbb1b3a97b55ffc6505
zBabEs <32611645+prapawit201@users.noreply.github.com>
2021-01-24 12:08:28 +0700

    [Create] Lab1 Restful API

---
+++ Lab/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 8080
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const routes = require('./router/indexRoutes')
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(routes)
+
+app.listen(PORT,()=>{
+    console.log('start server port : '+PORT);
+})
---
+++ Lab/router/indexRoutes.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const router = express.Router();
+
+let folder = [];
+let count = 0;
+
+router.get("/", function (req, res) {
+  res.json({
+    data: folder,
+  });
+  console.log("Get data success");
+});
+router.post("/test", function (req, res) {
+  let text = req.body.text;
+  let count = 0;
+  let length = folder.length;
+
+  if (length != 0) {
+    let hasmsg;
+    for (let index = 0; index < folder.length; index++) {
+      let textArray = folder[index].data;
+      if (textArray == text) {
+        count = count + folder[index].count;
+        count++;
+        folder[index].count = count;
+        hasmsg = true;
+        break;
+      } else {
+        hasmsg = false;
+      }
+    }
+    if (!hasmsg) {
+      count++;
+      folder.push({
+        data: text,
+        count: count,
+      });
+    }
+    return res.send({
+      data: folder,
+    });
+  } else {
+    count++;
+    folder.push({
+      data: text,
+      count: count,
+    });
+    return res.send({
+      data: folder,
+    });
+  }
+});
+
+module.exports = router;
