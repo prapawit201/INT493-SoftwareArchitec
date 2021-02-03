bf617a6e020f814874e0d8d905faad78e86d3a86
zBabEs <32611645+prapawit201@users.noreply.github.com>
2021-02-03 16:40:11 +0700

    [Edit] configure terraform

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
   }
 }
 provider "azurerm" {
-  version = "=2.0.0"
+  version = "2.18.0"
   features {}
 }
 
@@ -22,13 +22,15 @@ resource "azurerm_virtual_network" "main" {
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
+  address_prefix     = ["10.0.2.0/24"]
+
 }
 
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefix     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_public_ip" "main" {
