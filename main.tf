ef5e1fe9d3d5bc89a408eaa38f01471edc28fa86
zBabEs <32611645+prapawit201@users.noreply.github.com>
2021-02-03 20:42:43 +0700

    [Edit] configure terraform

--- main.tf
+++ main.tf
@@ -1,17 +1,20 @@
+# Configure the Azure Provider
 provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
   version = "=2.40.0"
   features {}
 }
 
-
-resource "azurerm_resource_group" "main" {
-  name     = "lab1-resources"
-  location = "southeastasia"
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "Souteast Asia"
 }
 
-resource "azurerm_virtual_network" "main" {
-  name                = "lab1-network"
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
 }
