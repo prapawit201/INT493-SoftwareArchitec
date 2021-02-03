ae15b60fc5c604b2a2b9a920f56e838d931dd172
zBabEs <32611645+prapawit201@users.noreply.github.com>
2021-02-03 16:42:06 +0700

    initial commit

--- main.tf
+++ main.tf
@@ -22,8 +22,6 @@ resource "azurerm_virtual_network" "main" {
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  address_prefix     = ["10.0.2.0/24"]
-
 }
 
 resource "azurerm_subnet" "internal" {
