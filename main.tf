010ba3b2fba63e94bf169699c5d608e2de8420cb
zBabEs <32611645+prapawit201@users.noreply.github.com>
2021-02-04 14:52:28 +0700

    [Edit] configure file

--- main.tf
+++ main.tf
@@ -3,12 +3,12 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "main" {
-  name     = "lab1-test"
+  name     = "lab2-test"
   location = "Southeast Asia"
 }
 
 resource "azurerm_virtual_network" "main" {
-  name                = "lab1-test-network"
+  name                = "lab2-test-network"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
@@ -22,14 +22,14 @@ resource "azurerm_subnet" "internal" {
 }
 
 resource "azurerm_public_ip" "main" {
-  name                = "lab1-pip"
+  name                = "lab2-pip"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   allocation_method   = "Static"
 }
 
 resource "azurerm_network_interface" "main" {
-  name                = "lab1-nic"
+  name                = "lab2-nic"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
 
@@ -42,7 +42,7 @@ resource "azurerm_network_interface" "main" {
 }
 
 resource "azurerm_linux_virtual_machine" "main" {
-  name                            = "lab1-vm"
+  name                            = "lab2-vm"
   resource_group_name             = azurerm_resource_group.main.name
   location                        = azurerm_resource_group.main.location
   size                            = "Standard_B1s"
@@ -74,8 +74,9 @@ resource "azurerm_linux_virtual_machine" "main" {
       "git clone https://github.com/prapawit201/INT493-SoftwareArchitec.git",
       "cd INT493-SoftwareArchitec/Lab",
       "npm install",
-      "sudo npm install -g pm2",
-      "pm2 start index.js"
+      "sudo mv myapp.service /lib/systemd/system/myapp.service",
+      "sudo systemctl enable myapp.service",
+      "sudo systemctl start myapp.service"
     ]
 
     connection {
